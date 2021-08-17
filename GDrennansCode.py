14abcc7c4f3a7b342d2f8631bf0921a1c6f2e737
Rogue Lavellan <64570776+G-Drennan@users.noreply.github.com>
2021-08-17 04:02:41 +0000

    Made own code

--- GDrennansCode.py
+++ GDrennansCode.py
@@ -1,3 +1,6 @@
-Print("Type a string: ")
-string=input()
-print("You typed: "+string)
+print("Type the first digits of student ID: ")
+ID=int(input())
+print("You name: ")
+name=input()
+for x in range(0,ID+1):
+ print(name)
